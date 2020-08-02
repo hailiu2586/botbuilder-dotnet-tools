35d5490f8926d5a9f544c2b59a4b188308912752
Hai Liu <hailiu@gmail.com>
2020-08-02 15:54:25 -0700

    reusable arm templates

---
+++ Templates/readme.md
@@ -0,0 +1,3 @@
+# ARM deployment template building blocks
+
+1. [./serverfarm.json](./serverfarm.json) create shared app service plan for hosting runtimes, bots and QnA
--- docker/test.cmd
+++ docker/test.cmd
@@ -4,5 +4,5 @@ set BOT_PROJ=%1
 if '%BOT_PROJ%'=='' set BOT_PROJ=%cd%
 set BOT_PROJ
 set SASURI=""
-docker run --mount type=bind,source=%BOT_PROJ%,target=/bot -e AZ_SUB_ID=User-andreo -e AZ_GROUP_NAME=va-prod-test -e BOT_NAME=luis-qna-todo-bot -e QNA_BOT_NAME=TodoBotWithLuisAndQnA --restart on-failure:3 -it hailiu2586/botbuild:0.1.20 /bin/bash --init-file /scripts/bootstrap.sh
+docker run --name botbuild --mount type=bind,source=%BOT_PROJ%,target=/bot -e AZ_SUB_ID=User-andreo -e AZ_GROUP_NAME=va-prod-test -e BOT_NAME=luis-qna-todo-bot -e QNA_BOT_NAME=TodoBotWithLuisAndQnA --restart on-failure:3 -it hailiu2586/botbuild:0.1.20 /bin/bash --init-file /scripts/bootstrap.sh
 endlocal
